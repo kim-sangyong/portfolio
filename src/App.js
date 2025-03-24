8391d054a5e996ecf6eabb7522b2823e42ebfb9d
kim-sangyong <sdragon0416@gmail.com>
2025-03-24 15:39:23 +0900

    리액트 헬멧 삭제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { HelmetProvider } from 'react-helmet-async';
 import MainPage from "../src/pages/MainPage.jsx";
 import Header from "../src/components/Header.jsx";
 import Footer from "../src/components/Footer.jsx";
@@ -8,13 +7,11 @@ import Footer from "../src/components/Footer.jsx";
 
 function App() {
   return (
-    <HelmetProvider>
       <div className="App">
         <Header />
             <MainPage />
         <Footer />
       </div>
-    </HelmetProvider>
   );
 }
 
